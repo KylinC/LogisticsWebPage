16ae4cbe832282440b975d90c34521f13e93792c
KylinC <k1017856853@icloud.com>
2019-07-20 19:32:11 +0800

    add new page demo

--- DataSet/locations.py
+++ DataSet/locations.py
@@ -7,9 +7,7 @@ import sys
 
 sys.path.append(".")
 from DataGene.k_net_model import *
-city_num=10
-
-
+city_num=20
 
 def get_json_data():
     # res = [
@@ -20,34 +18,34 @@ def get_json_data():
     # get solver dictionary
 
 
-    # x_dict = opt_solve() 
+#     x_dict = opt_solve() 
 
-    # x_dict_keys = list(x_dict.keys())
+#     x_dict_keys = list(x_dict.keys())
 
-    # all_dict1 = {}
-    # all_dict2 = {}
-    # for i in range(21):
-    #     for j in range(21):
-    #         keyword=str(i)+"-"+str(j)
-    #         all_dict1[keyword]=[]
-    #         all_dict2[keyword]=[]
+#     all_dict1 = {}
+#     all_dict2 = {}
+#     for i in range(21):
+#         for j in range(21):
+#             keyword=str(i)+"-"+str(j)
+#             all_dict1[keyword]=[]
+#             all_dict2[keyword]=[]
 
-    # for tag_tuple in x_dict:
-    #     if(tag_tuple[4]==0 and abs(x_dict[tag_tuple]-1)<0.5):
-    #         keyword = str(tag_tuple[2])+"-"+str(tag_tuple[3])
-    #         value = [tag_tuple[0],tag_tuple[1]]
-    #         all_dict1[keyword].append(value)
-    #         all_dict1["20-"+str(tag_tuple[3])].append(value)
-    #         all_dict1[str(tag_tuple[2])+"-20"].append(value)
-    #         all_dict1["20-20"].append(value)
+#     for tag_tuple in x_dict:
+#         if(tag_tuple[4]==0 and abs(x_dict[tag_tuple]-1)<0.5):
+#             keyword = str(tag_tuple[2])+"-"+str(tag_tuple[3])
+#             value = [tag_tuple[0],tag_tuple[1]]
+#             all_dict1[keyword].append(value)
+#             all_dict1["20-"+str(tag_tuple[3])].append(value)
+#             all_dict1[str(tag_tuple[2])+"-20"].append(value)
+#             all_dict1["20-20"].append(value)
 
-    # for tag_tuple in x_dict:
-    #     if(tag_tuple[4]==1 and abs(x_dict[tag_tuple]-1)<0.5):
-    #         keyword = str(tag_tuple[2])+"-"+str(tag_tuple[3])
-    #         value = [tag_tuple[0],tag_tuple[1]]
-    #         all_dict2[keyword].append(value)
-    # print(all_dict1)
-    # return all_dict1
+#     for tag_tuple in x_dict:
+#         if(tag_tuple[4]==1 and abs(x_dict[tag_tuple]-1)<0.5):
+#             keyword = str(tag_tuple[2])+"-"+str(tag_tuple[3])
+#             value = [tag_tuple[0],tag_tuple[1]]
+#             all_dict2[keyword].append(value)
+#     print(all_dict1)
+#     return all_dict1
 
     return {'0-0': [], '0-1': [[0, 5], [5, 16], [16, 1]], '0-2': [], '0-3': [], '0-4': [], 
         '0-5': [[0, 5]], '0-6': [], '0-7': [[0, 7]], '0-8': [], '0-9': [], '0-10': [], 
--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template, request, url_for, Response
 import json
 from neo4j import GraphDatabase
 from DataSet.locations import get_json_data
+from DataSet.relationship import get_route_data
 
 app = Flask(__name__)
 
@@ -20,7 +21,13 @@ def not_found(error):
 @app.route('/data')
 def data():
     callback = request.args.get('callback')
-    json_data = get_json_data()
+    json_data = json.dumps(get_json_data())
+    return Response('{}({})'.format(callback, json_data))
+
+@app.route('/allroute',methods=['POST','GET'])
+def allroute():
+    callback = request.args.get('callback')
+    json_data = get_route_data()
     return Response('{}({})'.format(callback, json_data))
 
 if __name__ == "__main__":
